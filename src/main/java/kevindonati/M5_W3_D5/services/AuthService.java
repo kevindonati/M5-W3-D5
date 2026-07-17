b13789b4e8b5ffafcadf1f5111b0e42b1c24f9e2
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-17 13:26:01 +0200

    Create AuthService.java

---
+++ src/main/java/kevindonati/M5_W3_D5/services/AuthService.java
@@ -0,0 +1,28 @@
+package kevindonati.M5_W3_D5.services;
+
+import kevindonati.M5_W3_D5.entities.Utente;
+import kevindonati.M5_W3_D5.exceptions.UnauthorizedException;
+import kevindonati.M5_W3_D5.payloads.LoginDTO;
+import kevindonati.M5_W3_D5.repositories.UtenteRepository;
+import kevindonati.M5_W3_D5.security.JWTTools;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UtenteRepository utenteRepository;
+
+    @Autowired
+    private JWTTools jwtTools;
+
+    public String checkCredentialsAndGenerateToken(LoginDTO body) {
+        Utente found = this.utenteRepository.findByEmail(body.email()).orElseThrow(() -> new UnauthorizedException("Credenziali non valide"));
+
+        if (found.getPassword().equals(body.password())) {
+            return this.jwtTools.generateToken(found);
+        } else {
+            throw new UnauthorizedException("Credenziali sbagliate");
+        }
+    }
+}
