f8f882482cee3d4311bf9c582ecef3247cb2d71c
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-17 14:06:46 +0200

    errorshandler

--- src/main/java/kevindonati/M5_W3_D5/security/SecurityConfig.java
+++ src/main/java/kevindonati/M5_W3_D5/security/SecurityConfig.java
@@ -2,11 +2,17 @@ package kevindonati.M5_W3_D5.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
@@ -20,4 +26,11 @@ public class SecurityConfig {
 
         return httpSecurity.build();
     }
+
+    @Bean
+    public PasswordEncoder getBCrypt() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    
 }
